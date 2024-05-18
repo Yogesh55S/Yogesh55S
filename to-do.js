8fe17ff97708b5f3effe7a5dd2afd325f3486bf6
Yogesh55S <141666129+Yogesh55S@users.noreply.github.com>
2024-05-18 16:36:33 +0530

    Add files via upload

---
+++ to-do.js
@@ -0,0 +1,36 @@
+const input = document.getElementById("input-box");
+const list = document.getElementById("list-container");
+
+function addtask(){
+  if(input.value === ''){
+    alert("You must be write something ");
+  }else{
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    list.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  input.value = "";
+  savedata();
+}
+
+list.addEventListener("click", function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("check");
+    savedata();
+  }else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    savedata();
+  }
+},false);
+
+function savedata(){
+  localStorage.setItem("data",list.innerHTML);
+}
+
+function showlist(){
+  list.innerHTML = localStorage.getItem("data");
+}
+showlist();
